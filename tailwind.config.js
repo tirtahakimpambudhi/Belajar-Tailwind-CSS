a9ccd999d0a2465a5285b96e45184830e1cd9172
tirtahakimpambudhi <awebthp@gmail.com>
2024-04-09 21:42:36 +0700

    Update ./public/index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/*.{html,js}"],
+  content: ["./public/**/*.{html,js}","./public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing : {
+        13 : '3.25rem',
+      }
+    },
   },
   plugins: [],
 }
